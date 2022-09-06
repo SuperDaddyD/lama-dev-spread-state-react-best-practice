590b65a47ac27b32a8345f40f3c1269ec7d83c54
deinde dawodu <mr.dawodu@gmail.com>
2022-09-06 12:43:27 -0400

    Feature branch

--- spread-state-best-practice/src/App.js
+++ spread-state-best-practice/src/App.js
@@ -2,7 +2,7 @@
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
